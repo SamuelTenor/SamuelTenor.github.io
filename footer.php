26b7e208d8754465089091ed54eff906ac4b2b72
SamuelTenor <45833019+SamuelTenor@users.noreply.github.com>
2019-04-03 11:16:25 +0200

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,9 @@
+<div class="container-fluid padding">
+	<div class="row text-center padding">
+		<div class="col-12 social padding">
+			<a href="#"><i class="fab fa-facebook"></i></a>
+			<a href="#"><i class="fab fa-google-plus-g"></i></a>
+			<a href="#"><i class="fab fa-instagram"></i></a>
+		</div>
+	</div>
+</div>
